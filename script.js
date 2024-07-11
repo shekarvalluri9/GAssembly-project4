ca4e50eac68bb1d2bb3ab669ac48aa4e9e074bcc
shekar valluri <93124660+shekarvalluri9@users.noreply.github.com>
2024-07-11 10:23:18 +0930

    javascript chabges

---
+++ script.js
@@ -0,0 +1,20 @@
+function checkanswer(answer) {
+    var correctanswer = "Australia";
+    var feedbackElement = document.getElementById('feedback');
+    if (answer == correctanswer) {
+        feedbackElement.innerHTML = "Yeh!, that's correct answer! well done mate";
+    }
+    else{
+        feedbackElement.innerHTML = "No that's incorrect you may want to check again your answer!";
+    }
+}
+function checkanswer(answer) {
+    var correctanswer = "India";
+    var feedbackElement = document.getElementById('feedback');
+    if (answer == correctanswer) {
+        feedbackElement.innerHTML = "Yeh!, that's correct answer! well done mate";
+    }
+    else{
+        feedbackElement.innerHTML = "No that's incorrect you may want to check again your answer!";
+    }
+}
